b9d44da9a2950a80fd23d26c8f4e4268d7760a2d
Mathieu Amiot <m.amiot@otak-arts.com>
2015-04-13 23:13:40 +0200

    Moar empty code

--- src/Connectors/Connector.php
+++ src/Connectors/Connector.php
@@ -1,8 +1,37 @@
-namespace Sequelize\Dialects;
+<?php
 
+    /*
+     * Copyright 2015 Mathieu "OtaK_" Amiot <m.amiot@otak-arts.com> http://mathieu-amiot.fr/
+     *
+     * Licensed under the Apache License, Version 2.0 (the "License");
+     * you may not use this file except in compliance with the License.
+     * You may obtain a copy of the License at
+     *
+     *      http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing, software
+     * distributed under the License is distributed on an "AS IS" BASIS,
+     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     * See the License for the specific language governing permissions and
+     * limitations under the License.
+     *
+     */
 
-abstract class Dialect
-{
+    /**
+     * @package    Sequelize
+     * @subpackage Connectors
+     * @author     Mathieu AMIOT <m.amiot@otak-arts.com>
+     * @copyright  Copyright (c) 2015, Mathieu AMIOT
+     * @version    1.0
+     * @changelog
+     *      1.0: Initial stable version
+     */
 
-}
+    namespace Sequelize\Connectors;
 
+    // TODO use statements
+
+    abstract class Connector
+    {
+
+    }
